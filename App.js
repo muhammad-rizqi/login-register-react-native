7a82c2f73ffe7cb41bb14046b457bae1ec935fe7
Muhammad Rizqi <muhammad-rizqi@live.com>
2020-10-06 05:37:41 +0700

    latihan

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import Login from './src/screen/Login';
+import Register from './src/screen/Register';
+
+export default class App extends Component {
+  render() {
+    return <Login />;
+  }
+}
